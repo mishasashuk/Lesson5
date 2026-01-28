560dac1da6832957e3ed1e1dfd2a156a099fbbb5
Alexandr Mishuk <mishasashuk@gmail.com>
2026-01-28 20:24:15 +0300

    feat: create function sumOfNumber

--- task1.ts
+++ task1.ts
@@ -1,1 +1,10 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+
+function sumOfNumbers(lastNumber: number = 4) {
+  let sum = 0;
+  for (let startNumber = 0; startNumber <= lastNumber; startNumber++) {
+    sum += startNumber;
+  }
+  console.log(sum);
+}
+sumOfNumbers();
