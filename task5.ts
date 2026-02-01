0e541b86efe72c6859e25c4353427e345b97d4ff
Alexandr Mishuk <mishasashuk@gmail.com>
2026-02-01 17:10:51 +0300

    resolve task5

--- task5.ts
+++ task5.ts
@@ -2,16 +2,21 @@
 // Саму задачу обернуть в отдельную функцию getDate, которая принимает в качестве параметра произвольную дату в формате '2026-10-22T22:10:15'
 //* Проверить валидна ли дата в переданном параметре
 
-const now: Date = new Date();
+function padZero(num: number): string {
+  return num.toString().padStart(2, "0");
+}
 
-const day = now.getDate().toString().padStart(2, "0");
-const month = (now.getMonth() + 1).toString().padStart(2, "0");
-const year = now.getFullYear();
+function getDate(dateString: string): string {
+  const now: Date = new Date(dateString);
 
-const hours = now.getHours().toString().padStart(2, "0");
-const minutes = now.getMinutes().toString().padStart(2, "0");
-const seconds = now.getSeconds().toString().padStart(2, "0");
+  const day = padZero(now.getDate());
+  const month = padZero(now.getMonth() + 1);
+  const year = padZero(now.getFullYear());
+  const hours = padZero(now.getHours());
+  const minutes = padZero(now.getMinutes());
+  const seconds = padZero(now.getSeconds());
 
-const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-
-console.log(formattedDate);
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+const formattedDate = new Date().toString();
+console.log(getDate(formattedDate));
